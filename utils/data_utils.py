bed4d89d0913544531ad184fee6880303f4d789b
michalwietecki <01180649@pw.edu.pl>
2026-03-14 13:40:14 +0100

    Data utils file

---
+++ utils/data_utils.py
@@ -0,0 +1,88 @@
+import torch
+import torchvision
+import torchvision.transforms as T
+from torch.utils.data import DataLoader, Subset
+import numpy as np
+import os
+
+# constannts from config file
+from utils.config import DATA_DIR, CINIC_MEAN, CINIC_STD, IMAGE_SIZE
+
+def get_transforms(augment_type="basic"):
+    base_tf = [
+        T.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+        T.ToTensor(),
+        T.Normalize(mean=CINIC_MEAN, std=CINIC_STD)
+    ]
+
+    if augment_type == "advanced":
+        advanced_tf = [
+            T.RandomHorizontalFlip(p=0.5),
+            T.RandomGrayscale(p=0.2),
+            T.RandomInvert(p=0.2),
+            T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))
+        ]
+        return T.Compose(advanced_tf + base_tf)
+    
+    return T.Compose(base_tf)
+
+# specific data loaders for different stages of experiments
+def get_dataloaders(config, split='train'):
+    aug_type = config.get("augmentations", "basic")
+    batch_size = config.get("batch_size", 64)
+    subset_ratio = config.get("subset_ratio", 1.0)
+    
+    transform = get_transforms(aug_type)
+    
+    path = os.path.join(DATA_DIR, split)
+    full_dataset = torchvision.datasets.ImageFolder(root=path, transform=transform)
+    
+    # few-shot mechanism
+    if split == 'train' and subset_ratio < 1.0:
+        num_samples = int(len(full_dataset) * subset_ratio)
+        
+        # random indices
+        indices = torch.randperm(len(full_dataset))[:num_samples]
+        dataset = Subset(full_dataset, indices)
+        print(f"Few-shot mode: Używam {num_samples} obrazów ({subset_ratio*100}% zbioru).")
+    else:
+        dataset = full_dataset
+
+    loader = DataLoader(
+        dataset, 
+        batch_size=batch_size, 
+        shuffle=(split == 'train'),
+        num_workers=2,
+        pin_memory=True # for gpu optimization
+    )
+    
+    return loader
+
+def apply_cutmix(inputs, targets, alpha=1.0):
+    if inputs.size(0) <= 1:
+        return inputs, targets, targets, 1.0
+
+    indices = torch.randperm(inputs.size(0))
+    shuffled_inputs = inputs[indices]
+    shuffled_targets = targets[indices]
+
+    lam = np.random.beta(alpha, alpha)
+    
+    W, H = inputs.size()[2], inputs.size()[3]
+    cut_rat = np.sqrt(1. - lam)
+    cut_w = int(W * cut_rat)
+    cut_h = int(H * cut_rat)
+
+    cx = np.random.randint(W)
+    cy = np.random.randint(H)
+
+    bbx1 = np.clip(cx - cut_w // 2, 0, W)
+    bby1 = np.clip(cy - cut_h // 2, 0, H)
+    bbx2 = np.clip(cx + cut_w // 2, 0, W)
+    bby2 = np.clip(cy + cut_h // 2, 0, H)
+
+    inputs[:, :, bbx1:bbx2, bby1:bby2] = shuffled_inputs[:, :, bbx1:bbx2, bby1:bby2]
+    
+    lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (W * H))
+    
+    return inputs, targets, shuffled_targets, lam
