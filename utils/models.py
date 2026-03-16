52e3124063042395076342b89eddb3dae76b197b
michalwietecki <01180649@pw.edu.pl>
2026-03-16 15:46:54 +0100

    More complex CNN model

--- utils/models.py
+++ utils/models.py
@@ -2,40 +2,61 @@ import torch
 import torch.nn as nn
 from torchvision import models
 
-# simple baseline CNN
+# baseline CNN
 class BaselineCNN(nn.Module):
     def __init__(self, num_classes=10, dropout_rate=0.0):
         super(BaselineCNN, self).__init__()
         num_classes = int(num_classes)
         
         self.features = nn.Sequential(
-            nn.Conv2d(3, 32, 3, padding=1), nn.ReLU(), nn.MaxPool2d(2),
-            nn.Conv2d(32, 64, 3, padding=1), nn.ReLU(), nn.MaxPool2d(2),
-            nn.Conv2d(64, 128, 3, padding=1), nn.ReLU(), nn.MaxPool2d(2),
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
         )
         
-        flattened_size = int(128 * 28 * 28)
+        self.flattened_size = 256 * 28 * 28 
         
         self.classifier = nn.Sequential(
             nn.Flatten(),
             nn.Dropout(p=float(dropout_rate)),
-            nn.Linear(flattened_size, 256),
+            nn.Linear(self.flattened_size, 512),
             nn.ReLU(),
-            nn.Linear(256, num_classes)
+            nn.Dropout(p=float(dropout_rate)),
+            nn.Linear(512, num_classes)
         )
 
     def forward(self, x):
-        return self.classifier(self.features(x))
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
 
 # Model factory
     
 def get_model(model_name, config):
-    # Wyciągamy wartości, podając domyślne, jeśli ich nie ma w configu
     num_classes = 10 
     dropout = config.get("dropout", 0.0)
 
     if model_name == "baseline_cnn":
-        # PRZEKAZUJEMY WARTOŚCI, A NIE CAŁY SŁOWNIK 'config'
         return BaselineCNN(num_classes=num_classes, dropout_rate=dropout)
     
     elif "efficientnet_b0" in model_name:
