81e1cbc5ecb7f57623d61ca9056ddcb7e29feb78
michalwietecki <01180649@pw.edu.pl>
2026-03-25 20:28:39 +0100

    Back to pin_memory=true

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -54,7 +54,7 @@ def get_dataloaders(config, split='train', num_workers=4):
         batch_size=batch_size,
         shuffle=(split == 'train'),
         num_workers=num_workers,
-        pin_memory=False # for gpu optimization
+        pin_memory=True # for gpu optimization
     )
     
     return loader
